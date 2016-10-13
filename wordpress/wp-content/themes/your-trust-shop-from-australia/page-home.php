67c4c3d16ff9a71358cd960b4a8aa5cc1921c08f
qiuxan <qiuxan@gmail.com>
2016-10-13 16:16:50 +1100

    our features

--- wordpress/wp-content/themes/your-trust-shop-from-australia/page-home.php
+++ wordpress/wp-content/themes/your-trust-shop-from-australia/page-home.php
@@ -23,8 +23,14 @@ $title3 		=get_field('title3');
 $information_content3 		=get_field('information_content3');
 $title4 		=get_field('title4');
 $information_content4 		=get_field('information_content4');
+$our_feature_image 		=get_field('our_feature_image');
 
-//print_r($notebook_image);
+$our_feature_title 		=get_field('our_feature_title');
+$our_features_body 		=get_field('our_features_body');
+
+
+//our_feature_image  our_feature_title	
+//print_r($notebook_image);our_features_body
 
 get_header(); ?>
 
@@ -84,7 +90,7 @@ get_header(); ?>
 				<?php    
 					if(!empty($notebook_image)):?>
 					<img src="<?php echo $notebook_image['url'];?>" alt="<?php echo $notebook_image['alt'];?>">
-					<?php endif;?>
+				<?php endif;?>
 				
 				
 				
@@ -120,41 +126,50 @@ get_header(); ?>
 		<div class="container">
 			
 			<div class="section-header">
-				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
-				<h2>快速链接</h2>
+				<!--$our_feature_image 		=get_field('our_feature_image');	$our_feature_title 		=get_field('our_feature_title');
+-->
+				
+				<!--if feature img exist-->
+				
+				<?php    
+					if(!empty($notebook_image)):?>
+					<img src="<?php echo $our_feature_image['url'];?>" alt="<?php echo $our_feature_image['alt'];?>">
+				<?php endif;?>
+								
+				
+				<h2><?php echo $our_feature_title;?></h2>
+				
+			
+				
+				<?php    
+					if(!empty($our_features_body)):?>
+					<p class="lead"><?php echo $our_features_body;?></p>
+				<?php endif;?>
+
 
 			</div> <!--end section header-->
 			<div class="row">
 				
-				<div class="col-sm-2">
-					<i class="ci ci-computer"></i> <!--font awesome-->
-					<h4>Lifetime access to 80+ lectures</h4>
-				</div><!-- end col -->
+				
+				<?php
+					$loop= new WP_Query(array('post_type'=>'our_features','orderby'=>'post_id','order'=>'ASC'));
+					//print_r($loop);
+				?>
+				
+				
+				<?php while($loop->have_posts()): $loop->the_post();?>
 				
 				<div class="col-sm-2">
-					<i class="ci ci-watch"></i>
-					<h4>10+ hours of HD video content</h4>
+					<i class="<?php the_field('our_feature_icon');?>"></i> <!--font awesome-->
+					<h4> <?php the_title();?></h4>
 				</div><!-- end col -->
 				
-				<div class="col-sm-2">
-					<i class="ci ci-calendar"></i>
-					<h4>30-day money back guarantee</h4>
-				</div><!-- end col -->
+				<?php endwhile; ?>
 				
-				<div class="col-sm-2">
-					<i class="ci ci-community"></i>
-					<h4>Access to a community of like-minded students</h4>
-				</div><!-- end col -->
 				
-				<div class="col-sm-2">
-					<i class="ci ci-instructor"></i>
-					<h4>Direct access to the instructor</h4>
-				</div><!-- end col -->
 				
-				<div class="col-sm-2">
-					<i class="ci ci-device"></i>
-					<h4>Accessible content on your mobile devices</h4>
-				</div><!-- end col -->
+				
+
 
 				
 			</div> <!--row-->
